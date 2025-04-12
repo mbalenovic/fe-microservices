f229ce71cada2efbd25ca6274b9e106fd03318e2
mbalenovic <mile.balenovic@gmail.com>
2025-04-12 21:15:55 +0200

    add auth middleware

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,31 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 const app = express();
 const PORT = 4000;
 const SECRET = "supersecret";
+
+// Authorization middleware
+const authorize = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  try {
+    const user = jwt.verify(token, SECRET);
+    req.user = user as JwtPayload;
+    next();
+  } catch {
+    res.status(401).json({ error: "Unauthorized" });
+  }
+};
 
 app.use(express.json());
 app.use(cookieParser());
@@ -33,14 +53,8 @@ app.post("/login", (req, res) => {
   res.json({ success: true });
 });
 
-app.get("/me", (req, res) => {
-  const token = req.cookies.token;
-  try {
-    const user = jwt.verify(token, SECRET);
-    res.json(user);
-  } catch {
-    res.status(401).json({ error: "Unauthorized" });
-  }
+app.get("/me", authorize, (req, res) => {
+  res.json(req.user);
 });
 
 app.listen(PORT, () =>
