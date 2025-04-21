6483fa6b76af74e6ec2a1cc92ec87e687bb7ac03
mbalenovic <mile.balenovic@gmail.com>
2025-04-21 19:44:38 +0200

    add maplibre-gl

--- micro-react/src/features/Map/Map.tsx
+++ micro-react/src/features/Map/Map.tsx
@@ -1,53 +1,48 @@
 import { socket } from "../../socket";
-import { useState, useEffect } from "react";
+import { useRef, useEffect } from "react";
+import { Map as MaplibreMap } from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
 
-export function ConnectionManager() {
-  function connect() {
-    console.log("Connecting to socket", socket);
-    socket.connect();
-  }
-
-  function disconnect() {
-    console.log("Disconnecting from socket", socket);
-    socket.disconnect();
-  }
-
-  return (
-    <div>
-      <button onClick={connect}>Connect</button>
-      <button onClick={disconnect}>Disconnect</button>
-    </div>
-  );
-}
-
-const Map = () => {
-  const [isConnected, setIsConnected] = useState(socket.connected);
+const ReactMap = () => {
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
+  const mapRef = useRef<MaplibreMap | null>(null);
 
   useEffect(() => {
-    function onConnect() {
-      setIsConnected(true);
+    if (mapContainerRef.current) {
+      mapRef.current = new MaplibreMap({
+        container: mapContainerRef.current,
+        style: "https://demotiles.maplibre.org/style.json", // public style, change if needed
+        center: [0, 0],
+        zoom: 2,
+      });
     }
 
-    function onDisconnect() {
-      setIsConnected(false);
-    }
-
-    socket.on("connect", onConnect);
-    socket.on("disconnect", onDisconnect);
+    mapRef.current?.on("move", () =>
+      socket.emit("map", {
+        center: mapRef.current?.getCenter(),
+        zoom: mapRef.current?.getZoom(),
+      })
+    );
 
     return () => {
-      socket.off("connect", onConnect);
-      socket.off("disconnect", onDisconnect);
+      if (mapRef.current) {
+        mapRef.current.remove();
+        mapRef.current = null;
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.connect();
+
+    return () => {
+      socket.disconnect();
     };
   }, []);
 
   return (
-    <div>
-      <button onClick={() => {}}>Test Event</button>
-      <ConnectionManager />
-      <p>Socket Status: {isConnected ? "Connected" : "Disconnected"}</p>
-    </div>
+    <div ref={mapContainerRef} style={{ width: "100%", height: "20vh" }} />
   );
 };
 
-export default Map;
+export default ReactMap;
